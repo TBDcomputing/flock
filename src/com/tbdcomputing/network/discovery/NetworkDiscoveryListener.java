e513fb89ee47b5b522296c53f67e4b30ef359a00
Andriy Katkov <akatkov@vt.edu>
2016-02-19 15:14:05 -0500

    remove address and corresponding exception

--- src/com/tbdcomputing/network/discovery/NetworkDiscoveryListener.java
+++ src/com/tbdcomputing/network/discovery/NetworkDiscoveryListener.java
@@ -5,7 +5,7 @@ import java.net.*;
 public class NetworkDiscoveryListener implements Runnable {
     public void run() {
         try {
-            DatagramSocket socket = new DatagramSocket(8888, InetAddress.getByName("255.255.255.255"));
+            DatagramSocket socket = new DatagramSocket(8888);
             while (true) {
                 try {
                     byte[] buf = new byte[1000];
@@ -16,9 +16,6 @@ public class NetworkDiscoveryListener implements Runnable {
                     System.err.println(e.getMessage());
                 }
             }
-        } catch (UnknownHostException e1) {
-            System.out.println("Failed to find host 255.255.255.255");
-            e1.printStackTrace();
         } catch (SocketException e1) {
             System.out.println("Failed to create socket.");
             e1.printStackTrace();
