e90a37fafbb3ddeac39c75216a972b1d7b094b87
Drew Sorrels <drew@registria.com>
2016-02-19 15:27:16 -0500

    fixed comparison to same address for listening and sending response

--- src/com/tbdcomputing/network/discovery/NetworkDiscoveryListener.java
+++ src/com/tbdcomputing/network/discovery/NetworkDiscoveryListener.java
@@ -3,37 +3,47 @@ package com.tbdcomputing.network.discovery;
 import java.net.*;
 
 public class NetworkDiscoveryListener implements Runnable {
-    public void run() {
-        try {
-            DatagramSocket socket = new DatagramSocket(8888, InetAddress.getByName("255.255.255.255"));
-            while (true) {
-                try {
-                    byte[] buf = new byte[1000];
-                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                    socket.receive(packet);
-                    System.out.println("Recieved a packet!");
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        } catch (UnknownHostException e1) {
-            System.out.println("Failed to find host 255.255.255.255");
-            e1.printStackTrace();
-        } catch (SocketException e1) {
-            System.out.println("Failed to create socket.");
-            e1.printStackTrace();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
+	public void run() {
+		try {
+			DatagramSocket socket = new DatagramSocket(8888);
+			while (true) {
+				try {
+					byte[] buf = new byte[1000];
+					DatagramPacket packet = new DatagramPacket(buf, buf.length);
+					socket.receive(packet);
+					System.out.println("Recieved a packet!");
 
+					// Don't respond to ourself because we will fail to bind to
+					// our same port
+					System.out.println(InetAddress.getLocalHost() + " | " + packet.getAddress());
 
-    public static void main(String[] args) {
-        new Thread() {
-            @Override
-            public void run() {
-                new NetworkDiscoveryListener().run();
-            }
-        }.start();
-    }
+					if (!InetAddress.getLocalHost().equals(packet.getAddress())) {
+
+						DatagramSocket sendSocket = new DatagramSocket(8888, packet.getAddress());
+						sendSocket.send(new DatagramPacket(buf, buf.length));
+						sendSocket.close();
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			// } catch (UnknownHostException e1) {
+			// System.out.println("Failed to find host 255.255.255.255");
+			// e1.printStackTrace();
+		} catch (SocketException e1) {
+			System.out.println("Failed to create socket.");
+			e1.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Thread() {
+			@Override
+			public void run() {
+				new NetworkDiscoveryListener().run();
+			}
+		}.start();
+	}
 }
