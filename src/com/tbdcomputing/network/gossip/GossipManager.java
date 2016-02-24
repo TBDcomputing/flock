45149788b601baacdbadc1217f54ae0360b92441
Andriy Katkov <akatkov@vt.edu>
2016-02-24 14:45:42 -0500

    add comments to GossipManager

--- src/com/tbdcomputing/network/gossip/GossipManager.java
+++ src/com/tbdcomputing/network/gossip/GossipManager.java
@@ -2,10 +2,16 @@ package com.tbdcomputing.network.gossip;
 
 import java.util.ArrayList;
 
+/**
+ * This manager class will keep track of the GossipNodes in the network.
+ */
 public class GossipManager {
     private ArrayList<GossipNode> nodes;
     private GossipNode me;
 
+    /**
+     * Initializes the local GossipNode info and adds it the list of nodes they know.
+     */
     public GossipManager() {
         me = new GossipNode();
         nodes = new ArrayList<>();
