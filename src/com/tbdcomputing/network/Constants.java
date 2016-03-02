a6f6aaeda0bd634d886d8c27f308dac38748a692
Andriy Katkov <akatkov@vt.edu>
2016-03-02 14:47:51 -0500

    more robust mac retrieval testing

--- src/com/tbdcomputing/network/Constants.java
+++ src/com/tbdcomputing/network/Constants.java
@@ -4,6 +4,9 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by akatkov on 2/22/16.
@@ -16,6 +19,7 @@ public class Constants {
 
     public static String getUUID() {
         try {
+            System.out.println("MAC Search: " + searchForMac());
             InetAddress address = InetAddress.getLocalHost();
 
             /*
@@ -35,6 +39,7 @@ public class Constants {
                     for (int i = 0; i < mac.length; i++) {
                         sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
                     }
+                    System.out.println("UUID: " + sb.toString());
                     return sb.toString();
                 } else {
                     System.out.println("Address doesn't exist or is not " +
@@ -49,6 +54,39 @@ public class Constants {
         } catch (SocketException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+
+    public static String searchForMac() throws SocketException {
+        String firstInterface = null;
+        Map<String, String> addressByNetwork =  new HashMap<>();
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface network = networkInterfaces.nextElement();
+
+            byte[] bmac = network.getHardwareAddress();
+            if (bmac != null) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < bmac.length; i++){
+                    sb.append(String.format("%02X%s", bmac[i], (i < bmac.length - 1) ? "-" : ""));
+                }
+
+                if (!sb.toString().isEmpty()) {
+                    addressByNetwork.put(network.getName(), sb.toString());
+                    System.out.println("Address = "+sb.toString()+" @ ["+network.getName()+"] "+network.getDisplayName());
+                }
+
+                if (!sb.toString().isEmpty() && firstInterface == null) {
+                    firstInterface = network.getName();
+                }
+            }
+        }
+
+        if (firstInterface != null) {
+            return addressByNetwork.get(firstInterface);
+        }
+
         return null;
     }
 }
